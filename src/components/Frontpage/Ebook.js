51a1601e033510f6c8d31deb226ca6ac0a41eed5
Karel Boer <karel@Karels-MacBook-Pro.local>
2020-06-15 11:45:55 -0500

    change ebook content

--- src/components/Frontpage/Ebook.js
+++ src/components/Frontpage/Ebook.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Ebook = () => {
     return(
     <div className="ebook">
-        <h2>Cómo la tecnología ha cambiado el comportamiento del consumidor y el proceso de compra</h2>
+        <h2>El lapso de atención humana <span>solo dura 8 segundos.</span></h2>
+        <h3> Su sitio web no solo crea una <span>impresión instantánea</span>, sino que también es como una tienda minorista<br/> que utiliza escaparates para <span>convencer a las personas</span> de que entren por la puerta.</h3>
         {/* <button>Baja Ebook</button> */}
     </div>
     )
